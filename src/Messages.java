f15999a149856b632e71e0d9e3cf36f465add6ba
Noah Triche <noahthetriche@Noahs-Air.home>
2020-01-29 17:11:22 -0800

    Initial commit

---
+++ src/Messages.java
@@ -0,0 +1,13 @@
+public class Messages
+{
+    public static void main (String[] args)
+    {
+        Thought thinking = new Thought();
+        Advice speaking = new Advice();
+
+        thinking.message();
+
+        speaking.message();
+    }
+}
+//noah triche per 6
